ddd941f91ad6c6cfd4fa7fe7d2eff8055f151f3d
Boris Mikic <boris.blizzard@gmail.com>
2011-03-04 23:21:07 +0000

    - added VS 2010 project and solution with a very basic app structure (which will NOT be used) - added include and lib folders

---
+++ external/include/IL/config.h
@@ -0,0 +1,209 @@
+/* include/IL/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Altivec extension found */
+#undef ALTIVEC_GCC
+
+/* "Enable debug code features" */
+#undef DEBUG
+
+/* PPC_ASM assembly found */
+#undef GCC_PCC_ASM
+
+/* X86_64_ASM assembly found */
+#undef GCC_X86_64_ASM
+
+/* X86_ASM assembly found */
+#undef GCC_X86_ASM
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#undef HAVE_DLFCN_H
+
+/* Define to 1 if you have the <GLUT/glut.h> header file. */
+#undef HAVE_GLUT_GLUT_H
+
+/* Define to 1 if you have the <GL/glut.h> header file. */
+#undef HAVE_GL_GLUT_H
+
+/* Define to 1 if you have the <GL/glu.h> header file. */
+#undef HAVE_GL_GLU_H
+
+/* Define to 1 if you have the <GL/gl.h> header file. */
+#undef HAVE_GL_GL_H
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#undef HAVE_INTTYPES_H
+
+/* Define to 1 if you have the `m' library (-lm). */
+#undef HAVE_LIBM
+
+/* Define to 1 if you have the `z' library (-lz). */
+#undef HAVE_LIBZ
+
+/* Define to 1 if you have the <memory.h> header file. */
+#undef HAVE_MEMORY_H
+
+/* Define to 1 if you have the <OpenGL/glu.h> header file. */
+#undef HAVE_OPENGL_GLU_H
+
+/* Define to 1 if you have the <OpenGL/gl.h> header file. */
+#undef HAVE_OPENGL_GL_H
+
+/* Define if you have POSIX threads libraries and header files. */
+#undef HAVE_PTHREAD
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#undef HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#undef HAVE_STDLIB_H
+
+/* Define to 1 if you have the <strings.h> header file. */
+#undef HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#undef HAVE_STRING_H
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#undef HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#undef HAVE_SYS_TYPES_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#undef HAVE_UNISTD_H
+
+/* Use nonstandard varargs form for the GLU tesselator callback */
+#undef HAVE_VARARGS_GLU_TESSCB
+
+/* Define to 1 if you have the <windows.h> header file. */
+#undef HAVE_WINDOWS_H
+
+/* Support Allegro API */
+#undef ILUT_USE_ALLEGRO
+
+/* Support DirectX8 API */
+#undef ILUT_USE_DIRECTX8
+
+/* Support DirectX9 API */
+#undef ILUT_USE_DIRECTX9
+
+/* Define if system supports OpenGL API */
+#undef ILUT_USE_OPENGL
+
+/* Support SDL API */
+#undef ILUT_USE_SDL
+
+/* Support X11 API */
+#undef ILUT_USE_X11
+
+/* Support X11 XRender extension */
+#undef ILUT_USE_XRENDER
+
+/* Support X11 XShm extension */
+#undef ILUT_USE_XSHM
+
+/* Define if you want to build ILU part of DevIL (used in DevIL
+   examples/tests) */
+#undef ILU_ENABLED
+
+/* Define if you can support at least some ASM */
+#undef IL_INLINE_ASM
+
+
+/* bmp support (BMP is a standard Windows bitmap.) */
+#undef IL_NO_BMP
+
+/* jpeg support (JPEG is most common lossy format. libjpeg powered) */
+#undef IL_NO_JPG
+
+/* png support (PNG is a smart, loseless and even open format. libpng
+   powered.) */
+#undef IL_NO_PNG
+
+/* tga support (TGA (aka TARGA file format) has been used mainly in games. It
+   is open and simple.) */
+#undef IL_NO_TGA
+
+/* Define if you have nvidia texture tools library installed */
+#undef IL_USE_DXTC_NVIDIA
+
+/* Define if you have libsquish installed */
+#undef IL_USE_DXTC_SQUISH
+
+/* Use libjpeg without modification. always enabled. */
+#undef IL_USE_JPEGLIB_UNMODIFIED
+
+/* LCMS include without lcms/ support */
+#undef LCMS_NODIRINCLUDE
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#undef LT_OBJDIR
+
+/* Building on Mac OS X */
+#undef MAX_OS_X
+
+/* define if you have memalign memory allocation */
+#undef MEMALIGN
+
+/* define if you have mm_malloc.h header */
+#undef MM_MALLOC
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+#undef NO_MINUS_C_MINUS_O
+
+/* Name of package */
+#undef PACKAGE
+
+/* Define to the address where bug reports for this package should be sent. */
+#undef PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#undef PACKAGE_NAME
+
+/* Define to the full name and version of this package. */
+#undef PACKAGE_STRING
+
+/* Define to the one symbol short name of this package. */
+#undef PACKAGE_TARNAME
+
+/* Define to the version of this package. */
+#undef PACKAGE_VERSION
+
+/* define if you have posix_memalign memory allocation */
+#undef POSIX_MEMALIGN
+
+/* Define to necessary symbol if this constant uses a non-standard name on
+   your system. */
+#undef PTHREAD_CREATE_JOINABLE
+
+/* restric keyword available */
+#undef RESTRICT_KEYWORD
+
+/* SSE extension found */
+#undef SSE
+
+/* SSE2 extension found */
+#undef SSE2
+
+/* SSE3 extension found */
+#undef SSE3
+
+/* Define to 1 if you have the ANSI C header files. */
+#undef STDC_HEADERS
+
+/* define if you have valloc memory allocation */
+#undef VALLOC
+
+/* Memory must be vector aligned */
+#undef VECTORMEM
+
+/* Version number of package */
+#undef VERSION
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+#undef WORDS_BIGENDIAN
+
+/* Define to 1 if the X Window System is missing or not being used. */
+#undef X_DISPLAY_MISSING
---
+++ src/System/TransitionManager.h
@@ -0,0 +1,17 @@
+#ifndef SYSTEM_TRANSITIONMANAGER_H
+#define SYSTEM_TRANSITIONMANAGER_H
+
+namespace System
+{
+	class TransitionManager
+	{
+	public:
+		TransitionManager();
+		~TransitionManager();
+		
+	};
+	
+	extern TransitionManager* transitionManager;
+	
+}
+#endif
