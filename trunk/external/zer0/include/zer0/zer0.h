5ac1c95969a2a920759e791db0c4b315899b1507
boris.blizzard <boris.blizzard@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-05 09:18:52 +0000

    - added zer0 project for zer0division.dll

---
+++ trunk/external/zer0/include/zer0/zer0.h
@@ -0,0 +1,17 @@
+#ifndef ZER0_H
+#define ZER0_H
+
+#include <hltypes/hstring.h>
+
+#include "zer0Export.h"
+
+namespace zer0
+{
+	zer0FnExport bool init(int width, int height, bool fullscreen, chstr name, void (*logFunction)(chstr));
+	zer0FnExport bool destroy();
+	zer0FnExport void setLogFunction(void (*fnptr)(chstr));
+	zer0FnExport void log(chstr message, chstr prefix = "[zer0] ");
+	
+}
+
+#endif
