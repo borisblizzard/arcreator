ad84d6937995c60cbdb63753f3fc1c8bb8dbdd6e
ForeverZer0 <ForeverZer0@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-10 05:10:37 +0000

    Implemented the Rect class. Have an error I don't understand with the getWidth and getHeight functions. Some insight would be apprecitiated.

--- trunk/external/zer0/src/RGSS/Rect.cpp
+++ trunk/external/zer0/src/RGSS/Rect.cpp
@@ -4,11 +4,63 @@ namespace zer0
 {
 	namespace RGSS
 	{
-		/*
 		Rect::Rect()
 		{
+			this->set(0, 0, 0, 0);
 		}
-		*/
-	
+		void Rect::empty()
+		{
+			this->set(0, 0, 0, 0);
+		}
+
+		int Rect::getX()
+		{
+			return this->x;
+		}
+		int Rect::getY()
+		{
+			return this->y;
+		}
+		int getWidth()
+		{
+			// This causes an error. Says it cannot be used in non-static member function. Why?
+			// How is different from the x and y? Confusion with the parent?
+			//return this->width;
+		}
+		int getHeight()
+		{
+			// This too.
+			//return this->height; 
+		}
+
+		void Rect::setX(int value)
+		{
+			this->x = value;
+		}
+		void Rect::setY(int value)
+		{
+			this->x = value;
+		}
+		void Rect::setWidth(int value)
+		{
+			this->width = value;
+		}
+		void Rect::setHeight(int value)
+		{
+			this->height = value;
+		}
+
+		void Rect::set(int vx, int vy, int vw, int vh)
+		{
+			this->x = vx;
+			this->y = vy;
+			this->width = vw;
+			this->height = vh;
+		}
+
+		Rect::~Rect()
+		{
+			// ????
+		}
 	}
 }
