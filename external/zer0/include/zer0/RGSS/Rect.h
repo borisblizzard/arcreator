b082d29f4038f0ca92a2d2b9b3dd50ac1fd25b2d
ForeverZer0 <ForeverZer0@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-10 05:10:37 +0000

    Implemented the Rect class. Have an error I don't understand with the getWidth and getHeight functions. Some insight would be apprecitiated.

--- external/zer0/include/zer0/RGSS/Rect.h
+++ external/zer0/include/zer0/RGSS/Rect.h
@@ -2,7 +2,6 @@
 #define ZER0_RECT_H
 
 #include <gtypes/Rectangle.h>
-
 #include "zer0Export.h"
 
 namespace zer0
@@ -11,8 +10,27 @@ namespace zer0
 	{
 		class zer0Export Rect : public grect
 		{
-		};
+		public:
+			int x, y, width, height;
 
+			// constructors/destructors
+			Rect();
+			Rect(int valueX, int valueY, int valueWidth, int valueHeight);
+			~Rect();
+
+			void set(int valueX, int valueY, int valueWidth, int valueHeight);
+			void empty();
+
+			int getX();
+			int getY();
+			int getWidth();
+			int getHeight();
+
+			void setX(int valueX);
+			void setY(int valueY);
+			void setWidth(int valueWidth);
+			void setHeight(int valueHeight);
+		};
 	}
 }
 #endif
