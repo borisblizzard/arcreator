535ac673a365cfc8983651e199078aa9d8db0140
Ryex <Ryex@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-10 02:37:35 +0000

    - implemented the RGSS Table class. I created a get_data and a set_data member function that can be wrapped by ruby.

--- trunk/external/zer0/include/zer0/RGSS/Table.h
+++ trunk/external/zer0/include/zer0/RGSS/Table.h
@@ -10,9 +10,27 @@ namespace zer0
 		class zer0Export Table
 		{
 		public:
-			Table();
+			// data 
+			int xsize;
+			int ysize;
+			int zsize;
+			// getters & setters
+			int get_xsize();
+			int get_ysize();
+			int get_zsize();
+			// getter setter functions for table data
+			short get_data(int x, int y, int z);
+			void set_data(int x, int y, int z, short data);
+			// constructor / destructor
+			Table(int xsize, int ysize, int zsize);
 			~Table();
+			//functions
+			void resize(int xsize, int ysize, int zsize);
 
+		private:
+			// pointer for data
+			short *data;
+			
 		};
 
 	}
