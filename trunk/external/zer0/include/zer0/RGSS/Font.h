4275c637f9bb5d236c3828b967a363438bd9c962
Ryex <Ryex@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-10 08:55:37 +0000

    - implemented RGSS Font class

--- trunk/external/zer0/include/zer0/RGSS/Font.h
+++ trunk/external/zer0/include/zer0/RGSS/Font.h
@@ -2,6 +2,8 @@
 #define ZER0_FONT_H
 
 #include "zer0Export.h"
+#include "RGSS/Color.h"
+#include <hltypes/hstring.h>
 
 namespace zer0
 {
@@ -10,9 +12,37 @@ namespace zer0
 		class zer0Export Font
 		{
 		public:
+			// constructer and deconstructer
 			Font();
+			Font(hstr name);
+			Font(hstr name, int size);
 			~Font();
 
+			//data members
+			hstr name;
+			int size;
+			bool bold;
+			bool italic;
+			Color color;
+
+			//static members
+			static hstr default_name;
+			static int default_size;
+			static bool default_bold;
+			static bool default_italic;
+			static Color default_color;
+
+			//getters and setters
+			hstr get_name();
+			void set_name(hstr name);
+			int get_size();
+			void set_size(int size);
+			bool get_bold();
+			void set_bold(bool state);
+			bool get_italic();
+			void set_italic(bool state);
+			Color get_color();
+			void set_color(Color &color);
 		};
 
 	}
--- trunk/external/zer0/src/RGSS/Font.cpp
+++ trunk/external/zer0/src/RGSS/Font.cpp
@@ -5,12 +5,83 @@ namespace zer0
 	namespace RGSS
 	{
 	
+		hstr Font::default_name = "MS PGothic";
+		int Font::default_size = 22;
+		bool Font::default_bold = false;
+		bool Font::default_italic = false;
+		Color Font::default_color = Color(255.0f, 255.0f, 255.0f, 255.0f);
+
+		//constructers
 		Font::Font()
 		{
+			this->name = this->default_name;
+			this->size = this->default_size;
+			this->bold = this->default_bold;
+			this->italic = this->default_italic;
+			this->color = this->default_color;
 		}
-	
+		Font::Font(hstr name)
+		{
+			this->name = name;
+			this->size = this->default_size;
+			this->bold = this->default_bold;
+			this->italic = this->default_italic;
+			this->color = this->default_color;
+		}
+		Font::Font(hstr name, int size)
+		{
+			this->name = name;
+			this->size = size;
+			this->bold = this->default_bold;
+			this->italic = this->default_italic;
+			this->color = this->default_color;
+		}
+
+		// deconstructer
 		Font::~Font()
 		{
+		}
+
+		//getters and setters
+		hstr Font::get_name()
+		{
+			return this->name;
+		}
+		void Font::set_name(hstr name)
+		{
+			this->name = name;
+		}
+		int Font::get_size()
+		{
+			return this->size;
+		}
+		void Font::set_size(int size)
+		{
+			this->size = size;
+		}
+		bool Font::get_bold()
+		{
+			return this->bold;
+		}
+		void Font::set_bold(bool state)
+		{
+			this->bold = bold;
+		}
+		bool Font::get_italic()
+		{
+			return this->italic;
+		}
+		void Font::set_italic(bool state)
+		{
+			this->italic = italic;
+		}
+		Color Font::get_color()
+		{
+			return this->color;
+		}
+		void Font::set_color(Color &color)
+		{
+			this->color = color;
 		}
 	
 	}
