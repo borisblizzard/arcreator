111b6fdd4ff06ae504e521a79069739a47c4d488
boris.blizzard <boris.blizzard@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-10 19:37:59 +0000

    - normalized coding convention for Color, Tone and Rect - removed inheritance of Rect from gtypes::Rectangle

--- trunk/external/zer0/src/RGSS/Color.cpp
+++ trunk/external/zer0/src/RGSS/Color.cpp
@@ -11,35 +11,6 @@ namespace zer0
 			this->set(255.0f, 255.0f, 255.0f);
 		}
 
-		void Color::setRed(float value)
-		{
-			value = hclamp<float>(value, -255, 255);
-			this->red = value;
-		}
-		void Color::setGreen(float value)
-		{
-			value = hclamp<float>(value, -255, 255);
-			this->green = value;
-		}
-		void Color::setBlue(float value)
-		{
-			value = hclamp<float>(value, -255, 255);
-			this->blue = value;
-		}
-
-		float Color::getRed()
-		{
-			return this->red;
-		}
-		float Color::getGreen()
-		{
-			return this->green;
-		}
-		float Color::getBlue()
-		{
-			return this->blue;
-		}
-
 		Color::Color(float r, float g, float b, float a)
 		{
 			this->set(r, g, b, a);
@@ -47,6 +18,26 @@ namespace zer0
 
 		Color::~Color()
 		{
+		}
+
+		void Color::setRed(float value)
+		{
+			this->red = hclamp(value, -255.0f, 255.0f);
+		}
+
+		void Color::setGreen(float value)
+		{
+			this->green = hclamp(value, -255.0f, 255.0f);
+		}
+
+		void Color::setBlue(float value)
+		{
+			this->blue = hclamp(value, -255.0f, 255.0f);
+		}
+
+		void Color::setAlpha(float value)
+		{
+			this->alpha = hclamp(value, -255.0f, 255.0f);
 		}
 
 		void Color::set(float r, float g, float b, float a)
--- trunk/external/zer0/src/RGSS/Rect.cpp
+++ trunk/external/zer0/src/RGSS/Rect.cpp
@@ -8,57 +8,28 @@ namespace zer0
 		{
 			this->set(0, 0, 0, 0);
 		}
+
+		Rect::Rect(int x, int y, int width, int height)
+		{
+			this->set(x, y, width, height);
+		}
+
+		Rect::~Rect()
+		{
+		}
+
+		void Rect::set(int x, int y, int width, int height)
+		{
+			this->x = x;
+			this->y = y;
+			this->width = width;
+			this->height = height;
+		}
+
 		void Rect::empty()
 		{
 			this->set(0, 0, 0, 0);
 		}
 
-		int Rect::getX()
-		{
-			return this->x;
-		}
-		int Rect::getY()
-		{
-			return this->y;
-		}
-		int Rect::getWidth()
-		{
-			return this->width;
-		}
-		int Rect::getHeight()
-		{
-			return this->height; 
-			// you forgot to prefix it with Rect:: to place the finction in the right scope :P - ryex
-		}
-
-		void Rect::setX(int value)
-		{
-			this->x = value;
-		}
-		void Rect::setY(int value)
-		{
-			this->x = value;
-		}
-		void Rect::setWidth(int value)
-		{
-			this->width = value;
-		}
-		void Rect::setHeight(int value)
-		{
-			this->height = value;
-		}
-
-		void Rect::set(int vx, int vy, int vw, int vh)
-		{
-			this->x = vx;
-			this->y = vy;
-			this->width = vw;
-			this->height = vh;
-		}
-
-		Rect::~Rect()
-		{
-			// donconstructer, what to do when the object is deleated
-		}
 	}
 }
