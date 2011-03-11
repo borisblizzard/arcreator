e6cf71087157a2e39f042faea74df93e507b1933
game_guy <game_guy@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-11 01:59:59 +0000

    Added initialize methods Viewport(x, y, width, height); Viewport(rect);

--- trunk/external/zer0/include/zer0/RGSS/Viewport.h
+++ trunk/external/zer0/include/zer0/RGSS/Viewport.h
@@ -23,6 +23,9 @@ namespace zer0
 			int oy;
 			int z;
 
+			Viewport(int x, int y, int width, int height);
+			Viewport(Rect rect);
+
 			void setColor(float r, float g, float b, float a = 255.0f);
 			void setTone(float r, float g, float b, float gr = 255.0f);
 			void setRect(int x, int y, int width, int height);
