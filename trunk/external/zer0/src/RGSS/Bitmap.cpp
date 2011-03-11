6591438308b3707d87b7cbb9e4871b5ba3744e28
Ryex <Ryex@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-11 05:27:55 +0000

    - removed RGSS Hidden Classes folder - Blank implementation of Bitmap - Fixed header includes so that there were no build errors

--- trunk/external/zer0/src/RGSS/Bitmap.cpp
+++ trunk/external/zer0/src/RGSS/Bitmap.cpp
@@ -1,3 +1,6 @@
+#include "RGSS/Color.h"
+#include "RGSS/Font.h"
+#include "RGSS/Rect.h"
 #include "RGSS/Bitmap.h"
 
 namespace zer0
@@ -5,12 +8,64 @@ namespace zer0
 	namespace RGSS
 	{
 	
-		Bitmap::Bitmap()
+		Bitmap::Bitmap(hstr filename)
+		{
+		}
+
+		Bitmap::Bitmap(int width, int height)
 		{
 		}
 	
 		Bitmap::~Bitmap()
 		{
+		}
+			
+		void Bitmap::Blit(int x, int y, Bitmap src_bitmap, Rect src_rect, int opacity)
+		{
+		}
+			
+		void Bitmap::Clear()
+		{
+		}
+			
+		void Bitmap::DrawText(int x, int y, int width, int height, hstr str, int align)
+		{
+		}
+			
+		void Bitmap::DrawText(Rect rect, hstr str, int align)
+		{
+		}
+			
+		void Bitmap::FillRect(int x, int y, int width, int height, Color color)
+		{
+		}
+			
+		void Bitmap::FillRect(Rect rect, Color color)
+		{
+		}
+			
+		Color Bitmap::getPixel(int x, int y)
+		{
+			// default inplamentation REPLACE
+			return Color();
+		}
+			
+		void Bitmap::setPixel(int x, int y, Color color)
+		{
+		}
+			
+		void Bitmap::ChangeHue(int hue)
+		{
+		}
+			
+		void Bitmap::StretchBlt(Rect dest_rect, Bitmap src_bitmap, Rect src_rect, int opacity)
+		{
+		}
+			
+		Rect Bitmap::TextSize(hstr str)
+		{
+			// default inplamentation REPLACE
+			return Rect(0, 0, 32, 32);
 		}
 	
 	}
--- trunk/external/zer0/src/RGSS/Viewport.cpp
+++ trunk/external/zer0/src/RGSS/Viewport.cpp
@@ -1,4 +1,6 @@
 #include "RGSS/Color.h"
+#include "RGSS/Rect.h"
+#include "RGSS/Tone.h"
 #include "RGSS/Viewport.h"
 
 namespace zer0
