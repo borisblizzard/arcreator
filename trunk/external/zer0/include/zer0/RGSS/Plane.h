09ead0c10c411429b1290af229829515a1acc691
ForeverZer0 <ForeverZer0@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-12 22:00:09 +0000

    -updated Plane.h and Plane.cpp

--- trunk/external/zer0/include/zer0/RGSS/Plane.h
+++ trunk/external/zer0/include/zer0/RGSS/Plane.h
@@ -7,12 +7,73 @@ namespace zer0
 {
 	namespace RGSS
 	{
+		class Bitmap; // forward declaration of Bitmap
+		class Color; // forward declaration of Color
+		class Tone; // forward declaration of Tone
+		class Viewport; // forward declaration of Viewport
+
 		class zer0Export Plane
 		{
 		public:
+			// Public variables
+			/// @brief The plane's bitmap
+			Bitmap bitmap;
+			/// @brief Blend type used for the plane
+			short blend_type;
+			/// @brief Color blended with plane
+			Color color;
+			/// @brief The alpha value of the plane
+			float opacity;
+			/// @brief The X-coordinate of the plane's starting point
+			int ox;
+			/// @brief The Y-coordinate of the plane's starting point
+			int oy;
+			/// @brief The plane's color tone
+			Tone tone;
+			/// @brief The plane's visibility.
+			bool visible;
+			/// @brief The plane's Z-coordinate
+			float z;
+			/// @brief The plane's X-axis zoom level
+			float zoom_x;
+			/// @brief The plane's Y-axis zoom level
+			float zoom_y;
+			
+			// Getters/Setters
+			/// @brief Returns the viewport specified when initialized
+			Viewport getViewport() { return this->viewport; };
+			/// @brief Sets the sprite's bitmap
+			/// @param[in] bitmap Bitmap object to set
+			void setBitmap(Bitmap value);
+			/// @brief Sets the alpha value of sprite
+			/// param[in] Integer value of sprite opacity
+			void setOpacity(float value);
+			/// @brief Sets the sprite zoom on the x-axis
+			/// param[in] Zoom value. 1.0 denotes actual pixel size
+			void setZoomX(float value);
+			/// @brief Sets the sprite zoom on the y-axis
+			/// param[in] Zoom value. 1.0 denotes actual pixel size
+			void setZoomY(float value);
+
+			// Instance methods
+			/// @brief Frees the sprite
+			void dispose();
+			/// @brief Boolean value if sprite has been disposed
+			/// @note This function is missing the "?" that the RGSS method uses. Not sure how to implement.
+			bool disposed();
+
+			// Constructors/Deconstructors
+			/// @brief Basic constructor
 			Plane();
+			/// @brief Constructor to initialize with viewport
+			/// @param[in] viewport Specifies the viewport to use for this plane
+			Plane(Viewport value);
+			/// @brief Basic Deconstructor
 			~Plane();
 
+		private:
+			/// @brief Private value to store the viewport
+			Viewport viewport;
 		};
 
 	}
--- trunk/external/zer0/src/RGSS/Plane.cpp
+++ trunk/external/zer0/src/RGSS/Plane.cpp
@@ -1,4 +1,10 @@
+#include <hltypes/util.h>
+
 #include "RGSS/Plane.h"
+#include "RGSS/Bitmap.h"
+#include "RGSS/Color.h"
+#include "RGSS/Tone.h"
+#include "RGSS/Viewport.h"
 
 namespace zer0
 {
@@ -7,11 +13,42 @@ namespace zer0
 	
 		Plane::Plane()
 		{
+			this->viewport = new Viewport();
 		}
-	
+		Plane::Plane(Viewport value)
+		{
+			this->viewport = value;
+		}
 		Plane::~Plane()
 		{
+			/// @todo Implement deconstructor
 		}
-	
+
+		void Plane::setBitmap(Bitmap value)
+		{
+			this->bitmap = value;
+			/// @todo Include Exception handling
+		}
+		void Plane::setOpacity(float value)
+		{
+			this->opacity = hclamp(value, 0.0f, 255.0f);
+		}
+		void Plane::setZoomX(float value)
+		{
+			this->zoom_x = hclamp(value, 0.0f, value);
+		}
+		void Plane::setZoomY(float value)
+		{
+			this->zoom_y = hclamp(value, 0.0f, value);
+		}
+
+		void Plane::dispose()
+		{
+
+		}
+		bool Plane::disposed()
+		{
+
+		}
 	}
 }
