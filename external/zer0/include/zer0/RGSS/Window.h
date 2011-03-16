ca3601eebda3ce88d025fe84ee4ab5e2157c18f5
ForeverZer0 <ForeverZer0@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-16 02:41:21 +0000

    -interfaced RGSS::Window (INCOMPLETE) Still need to fully interface a couple methods on the C++ side.

--- external/zer0/include/zer0/RGSS/Window.h
+++ external/zer0/include/zer0/RGSS/Window.h
@@ -21,80 +21,33 @@ namespace zer0
 		public:
 			/// @brief Exposes this class to Ruby.
 			static void createRubyInterface();
-
-			/// @brief Boolean flag to denote if window is active.
-			bool active;
-			/// @brief The opacity of the background windowskin.
-			float back_opacity;
-			/// @brief The bitmap that the skin and font is drawn on.
-			Bitmap* contents;
-			/// @brief The opacity of the window's contents.
-			float contents_opacity;
-			/// @brief A rect gotten from the cursor's rectangle.
-			Rect cursor_rect;
-			/// @brief The height, in pixels, of the window.
-			int height;
-			/// @brief The opacity if the window and its contents.
-			float opacity;
-			/// @brief The X-coordinate of the sprite's starting point.
-			int ox;
-			/// @brief The Y-coordinate of the sprite's starting point.
-			int oy;
-			/// @brief Boolean flag to draw the "waiting for input" arrow.
-			bool pause;
-			/// @brief Boolean value if the background bitmap should be stretched or tiled.
-			bool stretch;
-			/// @brief Boolean flag if window is visible.
-			bool visible;
-			/// @brief Width, in pixels, of the window.
-			int width;
-			/// @brief The bitmap graphic to use as the windowskin source.
-			Bitmap* windowskin;
-			/// @brief The X-coordinate of the window.
-			int x;
-			/// @brief The Y-coordinate of the window.
-			int y;
-			/// @brief The Z-coordinate of the window.
-			int z;
-
+			/// @brief Frees the sprite.
+			static VALUE dispose(VALUE self);
 			/// @brief Basic constructor.
-			Window();
-			/// @brief Constructor to initialize with viewport.
-			/// @param[in] viewport Specifies the viewport to use for this sprite.
-			Window(Viewport* value);
-			/// @brief Basic Deconstructor.
-			~Window();
-
-			/// @brief Returns the viewport specified when the window was created.
-			Viewport* getViewport() { return this->viewport; };
-			/// @brief Set the back opacity, keeping in range (0 - 255).
-			/// @param[in] Float value of the back opacity.
-			void setBackOpacity(float value);
+			/// @param[in] viewport Viewport component.
+			static VALUE initialize(int argc, VALUE *argv, VALUE self);
+			/// @brief Returns true if disposed, else false.
+			static VALUE isDisposed(VALUE self);
+			/// @brief Set the back opacity.
+			/// @param[in] value Alpha component.
+			/// @note The value will be clamped between -255 and 255.
+			static VALUE setBackOpacity(VALUE self, VALUE value);
 			/// @brief Set the bitmap to use for the window's contents.
 			/// @param[in] value Bitmap used as contents.
-			void setContents(Bitmap* value);
-			/// @brief Set the window contents' opacity, keeping in range (0 - 255).
-			/// @param[in] value Float value of the contents' opacity.
-			void setContentsOpacity(float value);
-			/// @brief Sets the opacity if the window, keeping in range (0 - 255).
-			/// @param[in] value Float value of the window's opacity.
-			void setOpacity(float value);
+			static VALUE setContents(VALUE self, VALUE* value);
+			/// @brief Set the window contents' opacity.
+			/// @param[in] value Alpha component
+			/// @note The value will be clamped between -255 and 255.
+			static VALUE setContentsOpacity(VALUE self, VALUE value);
+			/// @brief Set the window overall opacity.
+			/// @param[in] value Alpha component
+			/// @note The value will be clamped between -255 and 255.
+			static VALUE setOpacity(VALUE self, VALUE value);
 			/// @brief Sets the source bitmap of the windowskin.
-			/// @param[in] value Bitmap source of the windowskin.
-			void setWindowskin(Bitmap* value);
-
-			/// @brief Frees the sprite.
-			void dispose();
-			/// @brief Boolean value if sprite has been disposed.
-			/// @note This function is missing the "?" that the RGSS method uses. Not sure how to implement.
-			bool disposed();
+			/// @param[in] value Bitmap component.
+			static VALUE setWindowskin(VALUE self, VALUE* value);
 			/// @brief Calls the update method.
-			void update();
-			
-		private:
-			/// @brief The window's viewport.
-			Viewport* viewport;
-
+			static VALUE update(VALUE self);
 		};
 	}
 }
--- external/zer0/src/RGSS/Window.cpp
+++ external/zer0/src/RGSS/Window.cpp
@@ -15,56 +15,90 @@ namespace zer0
 
 		void Window::createRubyInterface()
 		{
+			VALUE rb_cWindow = rb_define_class("Window", rb_cObject);
+			rb_define_attr(rb_cWindow, "active", 1, 1);
+			rb_define_attr(rb_cWindow, "back_opacity", 1, 0);
+			rb_define_attr(rb_cWindow, "contents", 1, 0);
+			rb_define_attr(rb_cWindow, "contents_opacity", 1, 0);
+			rb_define_attr(rb_cWindow, "cursor_rect", 1, 1);
+			rb_define_attr(rb_cWindow, "height", 1, 1);
+			rb_define_attr(rb_cWindow, "opacity", 1, 0);
+			rb_define_attr(rb_cWindow, "ox", 1, 1);
+			rb_define_attr(rb_cWindow, "oy", 1, 1);
+			rb_define_attr(rb_cWindow, "pause", 1, 1);
+			rb_define_attr(rb_cWindow, "stretch", 1, 1);
+			rb_define_attr(rb_cWindow, "viewport", 1, 0);
+			rb_define_attr(rb_cWindow, "visible", 1, 1);
+			rb_define_attr(rb_cWindow, "width", 1, 1);
+			rb_define_attr(rb_cWindow, "windowskin", 1, 0);
+			rb_define_attr(rb_cWindow, "x", 1, 1);
+			rb_define_attr(rb_cWindow, "y", 1, 1);
+			rb_define_attr(rb_cWindow, "z", 1, 1);
+			rb_define_method(rb_cWindow, "back_opacity=", RUBY_METHOD_FUNC(&Window::setBackOpacity), 1);
+			rb_define_method(rb_cWindow, "contents=", RUBY_METHOD_FUNC(&Window::setContents), 1);
+			rb_define_method(rb_cWindow, "contents_opacity=", RUBY_METHOD_FUNC(&Window::setContentsOpacity), 1);
+			rb_define_method(rb_cColor, "initialize", RUBY_METHOD_FUNC(&Color::initialize), -1);
+			rb_define_method(rb_cWindow, "opacity=", RUBY_METHOD_FUNC(&Window::setOpacity), 1);
+			rb_define_method(rb_cWindow, "windowskin=", RUBY_METHOD_FUNC(&Window::setWindowskin), 1);
+			rb_define_method(rb_cWindow, "dispose", RUBY_METHOD_FUNC(&Window::dispose), 0);
+			rb_define_method(rb_cWindow, "disposed?", RUBY_METHOD_FUNC(&Window::isDisposed), 0);
 		}
 	
-		Window::Window()
-		{
-			this->viewport = new Viewport();
-		}
-		Window::Window(Viewport* value)
-		{
-			this->viewport = value;
-		}
-		/// @todo implement
-		Window::~Window()
-		{
-		}
-	
-		void Window::setBackOpacity(float value)
-		{
-			this->back_opacity = hclamp(value, 0.0f, 255.0f);
-		}
 		/// @todo add RGSS Error calls
-		void Window::setContents(Bitmap* value)
+		/// @todo set values for changing alpha on C++ side
+
+		VALUE Window::initialize(int argc, VALUE *argv, VALUE self)
 		{
-			this->contents = value;
-		}
-		void Window::setContentsOpacity(float value)
-		{
-			this->contents_opacity = hclamp(value, 0.0f, 255.0f);
-		}
-		void Window::setOpacity(float value)
-		{
-			this->opacity = hclamp(value, 0.0f, 255.0f);
-		}
-		/// @todo add RGSS Error calls
-		void Window::setWindowskin(Bitmap* value)
-		{
-			this->windowskin = value;
+			if (argc > 1)
+				;// rb_raise(ArgumentError);
+			else if (argc == 1)
+				rb_iv_set(rb_cWindow, "@viewport", argv[0]);
+			return self;
 		}
 
-		void Window::dispose()
+		VALUE Window::dispose(VALUE self)
 		{
-
-		}
-		bool Window::disposed()
-		{
-			return true;
-		}
-		void Window::update()
-		{
-
+			/// @todo Implement dispose method
+			return Qnil;
 		}
 
+		VALUE Window::isDisposed(VALUE self)
+		{
+			/// @todo Implement disposed? method 
+			return Qnil;
+		}
+
+		VALUE Window::setBackOpacity(VALUE self, VALUE value)
+		{
+			value = rb_float_new(hclamp(NUM2DBL(value), -255.0, 255.0));
+			rb_iv_set(self, "@back_opacity", value);
+			return value;
+		}
+
+		VALUE Window::setContents(VALUE self, VALUE* value)
+		{
+			rb_iv_set(self, "@bitmap", *value);
+			return *value;
+		}
+
+		VALUE Window::setContentsOpacity(VALUE self, VALUE value)
+		{
+			value = rb_float_new(hclamp(NUM2DBL(value), -255.0, 255.0));
+			rb_iv_set(self, "@opacity", value);
+			return value;
+		}
+
+		VALUE Window::setOpacity(VALUE self, VALUE value)
+		{
+			value = rb_float_new(hclamp(NUM2DBL(value), -255.0, 255.0));
+			rb_iv_set(self, "@opacity", value);
+			return value;
+		}
+
+		VALUE Window::setWindowskin(VALUE self, VALUE* value)
+		{
+			rb_iv_set(self, "@windowskin", *value);
+			return *value;
+		}
 	}
 }
