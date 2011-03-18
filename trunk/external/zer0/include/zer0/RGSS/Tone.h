b711aecfb7b9a84458d6c6fdf3327355cde685c8
boris.blizzard <boris.blizzard@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-03-18 11:17:12 +0000

    - implemented RGSS::Tone - updated RGSS::Tone and RGSS::Color to match coding convention

--- trunk/external/zer0/include/zer0/RGSS/Tone.h
+++ trunk/external/zer0/include/zer0/RGSS/Tone.h
@@ -15,56 +15,71 @@ namespace zer0
 		class zer0Export Tone
 		{
 		public:
-			/// @brief Exposes this class to Ruby.
-			static void createRubyInterface();
+			/// @todo Dummy for now, needs to be removed later.
+			Tone() { }
+			/// @todo Dummy for now, needs to be removed later.
+			~Tone() { }
 
-			/// @brief Empty constructor.
-			Tone();
-			/// @brief Basic constructor.
+			/// @brief Sets the tone to the specified value.
 			/// @param[in] r Red component.
 			/// @param[in] g Green component.
 			/// @param[in] b Blue component.
 			/// @param[in] gr Gray component.
-			Tone(float r, float g, float b, float gr = 255.0f);
-			/// @brief Destructor.
-			~Tone();
+			/// @note Tone values will be clamped between -255 and 255.
+			/// @note Gray is clamped between 0 and 255.
+			void set(float r, float g, float b, float gr = 0.0f);
+
+			/// @brief Exposes this class to Ruby.
+			static void createRubyInterface();
+			/// @brief Ruby allocation of an instance.
+			static VALUE rb_new(VALUE classe);
+			/// @brief Sets the color to the specified value.
+			/// @param[in] argc Number of arguments.
+			/// @param[in] argv Pointer to first argument.
+			/// @note Arguments are "r, g, b[, a]".
+			/// @note Tone values will be clamped between -255 and 255.
+			/// @note Gray is clamped between 0 and 255.
+			static VALUE rb_initialize(int argc, VALUE* argv, VALUE self);
+			/// @brief Gets a string representation of the instance.
+			/// @return String representation of the instance.
+			static VALUE rb_inspect(VALUE self);
 
 			/// @brief Gets the red component.
 			/// @return Red component.
-			float getRed() { return this->red; }
-			/// @brief Gets the green component.
-			/// @return Green component.
-			float getGreen() { return this->red; }
-			/// @brief Gets the blue component.
-			/// @return Blue component.
-			float getBlue() { return this->red; }
-			/// @brief Gets the gray component.
-			/// @return Gray component.
-			float getGray() { return this->red; }
+			static VALUE rb_getRed(VALUE self);
 			/// @brief Sets the red component.
 			/// @param[in] value Red component.
 			/// @note The value will be clamped between -255 and 255.
-			void setRed(float value);
+			static VALUE rb_setRed(VALUE self, VALUE value);
+			/// @brief Gets the green component.
+			/// @return Green component.
+			static VALUE rb_getGreen(VALUE self);
 			/// @brief Sets the green component.
 			/// @param[in] value Green component.
 			/// @note The value will be clamped between -255 and 255.
-			void setGreen(float value);
+			static VALUE rb_setGreen(VALUE self, VALUE value);
+			/// @brief Gets the blue component.
+			/// @return Blue component.
+			static VALUE rb_getBlue(VALUE self);
 			/// @brief Sets the blue component.
 			/// @param[in] value Blue component.
 			/// @note The value will be clamped between -255 and 255.
-			void setBlue(float value);
+			static VALUE rb_setBlue(VALUE self, VALUE value);
+			/// @brief Gets the gray component.
+			/// @return Gray component.
+			static VALUE rb_getGray(VALUE self);
 			/// @brief Sets the gray component.
 			/// @param[in] value Gray component.
-			/// @note The value will be clamped between -255 and 255.
-			void setGray(float value);
+			/// @note The value will be clamped between 0 and 255.
+			static VALUE rb_setGray(VALUE self, VALUE value);
 
 			/// @brief Sets the color to the specified value.
-			/// @param[in] r Red component.
-			/// @param[in] g Green component.
-			/// @param[in] b Blue component.
-			/// @param[in] gr Gray component.
-			/// @note All values will be clamped between -255 and 255.
-			void set(float r, float g, float b, float gr = 255.0f);
+			/// @param[in] argc Number of arguments.
+			/// @param[in] argv Pointer to first argument.
+			/// @note Arguments are "r, g, b[, gr]".
+			/// @note Tone values will be clamped between -255 and 255.
+			/// @note Gray is clamped between 0 and 255.
+			static VALUE rb_set(int argc, VALUE* argv, VALUE self);
 
 		protected:
 			/// @brief Red component.
