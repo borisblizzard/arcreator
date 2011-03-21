7ba1e58e085a2273d028ac1aba501175248be658
Ryex <ryexander@gmail.com>
2011-03-21 00:40:51 +0000

    - implemented and interfaced RGSS::Table. unsure of the destructor and the code should be double checked.
    the project doesn't build because of the Tilemap class's use of Viewport and Table

--- external/zer0/include/zer0/RGSS/Table.h
+++ external/zer0/include/zer0/RGSS/Table.h
@@ -16,70 +16,53 @@ namespace zer0
 		class zer0Export Table
 		{
 		public:
+			static VALUE rb_cTable;
+			/// @todo Dummy for now, needs to be removed later.
+			Table();
+			// @todo Dummy for now, needs to be removed later.
+			~Table();
+
 			/// @brief Exposes this class to Ruby.
 			static void createRubyInterface();
-
-			/// @brief Basic constructor.
-			/// @param xSize X dimension size.
-			/// @param ySize Y dimension size.
-			/// @param zSize Z dimension size.
-			Table(int xSize, int ySize = 1, int zSize = 1);
-			// @brief Destructor
-			~Table();
+			/// @brief Ruby allocation of an instance.
+			static VALUE rb_new(VALUE classe);
+			/// @brief Sets the rect to the specified value.
+			/// @param[in] argc Number of arguments.
+			/// @param[in] argv Pointer to first argument.
+			static VALUE rb_initialize(int argc, VALUE* argv, VALUE self);
+			/// @brief Gets a string representation of the instance.
+			/// @return String representation of the instance.
+			static VALUE rb_inspect(VALUE self);
 
 			/// @brief Gets the X dimension size.
 			/// @return X dimension size.
-			int getXSize() const { return this->xSize; }
+			static VALUE rb_getXSize(VALUE self);
 			/// @brief Gets the Y dimension size.
 			/// @return Y dimension size.
-			int getYSize() const { return this->xSize; }
+			static VALUE rb_getYSize(VALUE self);
 			/// @brief Gets the Z dimension size.
 			/// @return Z dimension size.
-			int getZSize() const { return this->xSize; }
+			static VALUE rb_getZSize(VALUE self);
 
 			/// @brief Gets the value of an entry at a specific position in the table.
-			/// @param[in] x X position.
+			/// @param[in] argc Number of arguments.
+			/// @param[in] argv Pointer to first argument.
+			/// @note Arguments are "x[, y[, z]]".
 			/// @return Value at position.
-			/// @todo Add out-of-range exception
-			short getData(int x) const;
-			/// @brief Gets the value of an entry at a specific position in the table.
-			/// @param[in] x X position.
-			/// @param[in] y Y position.
-			/// @return Value at position.
-			/// @todo Add out-of-range exception
-			short getData(int x, int y) const;
-			/// @brief Gets the value of an entry at a specific position in the table.
-			/// @param[in] x X position.
-			/// @param[in] y Y position.
-			/// @param[in] z Z position.
-			/// @return Value at position.
-			/// @todo Add out-of-range exception
-			short getData(int x, int y, int z) const;
-			/// @brief Sets the value of an entry at a specific position in the table.
-			/// @param[in] x X position.
-			/// @param[in] value The new value.
-			/// @todo Add out-of-range exception
-			void setData(int x, short value);
-			/// @brief Sets the value of an entry at a specific position in the table.
-			/// @param[in] x X position.
-			/// @param[in] y Y position.
-			/// @param[in] value The new value.
-			/// @todo Add out-of-range exception
-			void setData(int x, int y, short value);
-			/// @brief Sets the value of an entry at a specific position in the table.
-			/// @param[in] x X position.
-			/// @param[in] y Y position.
-			/// @param[in] z Z position.
-			/// @param[in] value The new value.
-			/// @todo Add out-of-range exception
-			void setData(int x, int y, int z, short value);
+			static VALUE rb_getData(int argc, VALUE* argv, VALUE self);
 
+			/// @brief Sets the value of an entry at a specific position in the table.
+			/// @param[in] argc Number of arguments.
+			/// @param[in] argv Pointer to first argument.
+			/// @note Arguments are "x, value[, y[, z]]" where value is shifted right for each optional argument (it's allways the last argument).
+			static VALUE rb_setData(int argc, VALUE* argv, VALUE self);
+			
+			
 			/// @brief Resizes the Table to a new size.
-			/// @param xSize New X dimension size.
-			/// @param ySize New Y dimension size.
-			/// @param zSize New Z dimension size.
-			/// @note Entries at intersecting positions will remain in the table.
-			void resize(int xSize, int ySize = 1, int zSize = 1);
+			/// @param[in] argc Number of arguments.
+			/// @param[in] argv Pointer to first argument.
+			/// @note Arguments are "xSize[, ySize[, zSize]]".
+			static VALUE rb_resize(int argc, VALUE* argv, VALUE self);
 
 		protected:
 			/// @brief X dimension size.
@@ -97,6 +80,13 @@ namespace zer0
 			/// @param zSize Z dimension size.
 			/// @return Pointer to the newly allocated memory.
 			short* _createData(int xSize, int ySize, int zSize) const;
+
+			/// @brief Resizes the Table to a new size.
+			/// @param xSize New X dimension size.
+			/// @param ySize New Y dimension size.
+			/// @param zSize New Z dimension size.
+			/// @note Entries at intersecting positions will remain in the table.
+			void _resize(int xSize, int ySize = 1, int zSize = 1);
 			
 		};
 
