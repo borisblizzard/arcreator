51cc354c9a01cddb03dfc52309eb8c5c4702467b
Ryex <ryexander@gmail.com>
2011-03-25 01:19:37 +0000

    - just a small edit to ease my mind

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 puts "Using Ruby '#{RUBY_VERSION}' built for '#{RUBY_PLATFORM}' released on '#{RUBY_RELEASE_DATE}'\n\nHello World."
-puts 'Press ENTER to exit'
+puts 'Press ESCAPE to exit'
 s1 = Sprite.new
 
 def test
@@ -36,8 +36,8 @@ loop do
 	Graphics.update
 	Input.update
 	Graphics.frame_reset if Input.press?(Input::C)
-	s1.y = 100 + Graphics::frame_count % 20
-	s2.x = 100 + Graphics::frame_count % 20
+	s1.y = 100 + Graphics::frame_count % 40
+	s2.x = 100 + Graphics::frame_count % 40
 	break if Input.press?(Input::B)
 end
 
