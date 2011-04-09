1938ef4124a3c0f740dba531316a8bd3300f51b6
Ryex <ryexander@gmail.com>
2011-04-09 23:09:18 +0000

    - doing a slight restructure of the editor. removed the rather pointless cross platform path fixer and replaced it with os.path.normpath which dose the same thing but better. preparing to add a AUI window manager manager (yes a class to manage the manager, I'm going to abstract the interface so it is easier to use internally, direct access will still be possible) and phase out the RMXP operation mode.

--- editor/ARCed/src/Core/RMXP/__init__.py
+++ editor/ARCed/src/Core/RMXP/__init__.py
@@ -1,10 +1,7 @@
 """RMXP module, contains core components related to RMXP"""
 import Cache
-import Controls
 import Data
 import Dialogs
-import Layouts
 import RGSS1_RPG
 import RMPY
-import RMXPProject
 import RPGutil
