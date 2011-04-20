9d03d0d13cf5d0bd1cfdcab1744cd77cc2951f67
ForeverZer0 <ForeverZer0@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-04-20 03:00:01 +0000

    - Fixed Audio module to allow for optional arguments.

--- external/rgss/include/rgss/Audio.h
+++ external/rgss/include/rgss/Audio.h
@@ -19,40 +19,40 @@ namespace rgss
 		/// @brief Exposes this class to Ruby.
 		static void createRubyInterface();
 		/// @brief Starts BMG playback.
-		/// @param[in] filename Filename of the BGM to play.
-		/// @param[in] volume The volume of the BMG.
-		/// @param[in] pitch The pitch of the BGM.
-		static VALUE bgm_play(VALUE self, VALUE filename, VALUE volume, VALUE pitch);
+		/// @param[in] argc Number of arguments.
+		/// @param[in] argv Pointer to first argument.
+		/// @note Arguments are "filename[, volume[, pitch]]".
+		static VALUE bgm_play(int argc, VALUE* argv, VALUE self);
 		/// @brief Starts BMG fadeout.
 		/// @param[in] value The fadeout time in milliseconds.
 		static VALUE bgm_fade(VALUE self, VALUE value);
 		/// @brief Stops BGM playback.
 		static VALUE bgm_stop(VALUE self);
 		/// @brief Starts BGS playback.
-		/// @param[in] filename Filename of the BGS to play.
-		/// @param[in] volume The volume of the BGS.
-		/// @param[in] pitch The pitch of the BGS.
-		static VALUE bgs_play(VALUE self, VALUE filename, VALUE volume, VALUE pitch);
+		/// @param[in] argc Number of arguments.
+		/// @param[in] argv Pointer to first argument.
+		/// @note Arguments are "filename[, volume[, pitch]]".
+		static VALUE bgs_play(int argc, VALUE* argv, VALUE self);
 		/// @brief Starts BGS fadeout.
 		/// @param[in] value The fadeout time in milliseconds.
 		static VALUE bgs_fade(VALUE self, VALUE value);
 		/// @brief Stops BGS playback.
 		static VALUE bgs_stop(VALUE self);
 		/// @brief Starts ME playback.
-		/// @param[in] filename Filename of the ME to play.
-		/// @param[in] volume The volume of the ME.
-		/// @param[in] pitch The pitch of the ME.
-		static VALUE me_play(VALUE self, VALUE filename, VALUE volume, VALUE pitch);
+		/// @param[in] argc Number of arguments.
+		/// @param[in] argv Pointer to first argument.
+		/// @note Arguments are "filename[, volume[, pitch]]".
+		static VALUE me_play(int argc, VALUE* argv, VALUE self);
 		/// @brief Starts ME fadeout.
 		/// @param[in] value The fadeout time in milliseconds.
 		static VALUE me_fade(VALUE self, VALUE value);
 		/// @brief Stops ME playback.
 		static VALUE me_stop(VALUE self);
 		/// @brief Starts SE playback.
-		/// @param[in] filename Filename of the SE to play.
-		/// @param[in] volume The volume of the SE.
-		/// @param[in] pitch The pitch of the SE.
-		static VALUE se_play(VALUE self, VALUE filename, VALUE volume, VALUE pitch);
+		/// @param[in] argc Number of arguments.
+		/// @param[in] argv Pointer to first argument.
+		/// @note Arguments are "filename[, volume[, pitch]]".
+		static VALUE se_play(int argc, VALUE* argv, VALUE self);
 		/// @brief Stops SE playback.
 		static VALUE se_stop(VALUE self);
 	};
