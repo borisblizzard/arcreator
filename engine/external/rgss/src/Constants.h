41709a0db36c4c54da127047e5b949f1b4562b08
Boris Mikic <boris.blizzard@gmail.com>
2011-09-06 19:41:04 +0000

    - bumped version files for building to revision 307

--- engine/external/rgss/src/Constants.h
+++ engine/external/rgss/src/Constants.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR (0)
 #define VERSION_MINOR (9)
-#define VERSION_REVISION (1)
-#define VERSION_BUILD (289)
+#define VERSION_REVISION (2)
+#define VERSION_BUILD (307)
 
 #define cstr(s) #s
 #define cstr2(s) cstr(s)
