603e77b8528b260a69f66b450322bd1386b287b4
ForeverZer0 <ForeverZer0@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-10-13 02:41:07 +0000

    - Added functionality to load the pages for the database tab control by reading values in an external .ini file - Moved imports from __main__ to the necessary files (main panels only)

--- trunk/editor/ARCed/tests/panels/ARCedPanelTest/src/__main__.py
+++ trunk/editor/ARCed/tests/panels/ARCedPanelTest/src/__main__.py
@@ -3,127 +3,58 @@ cmd_folder = os.path.dirname(os.path.abspath(__file__))
 if cmd_folder not in sys.path:
 	sys.path.insert(0, cmd_folder)
 
+import ConfigParser
+
 import wx
 import ARCed_Templates
-import ARCedActorParameters_Dialog
 import ARCedActors_Panel
-import ARCedAnimationCellBatch_Dialog
-import ARCedAnimationCellProperties_Dialog
-import ARCedAnimationEntireSlide_Dialog
-import ARCedAnimations_Panel
-import ARCedAnimationTiming_Dialog
-import ARCedAnimationTweening_Dialog
 import ARCedArmors_Panel
-import ARCedBattleAnimation_Dialog
-import ARCedBattleProcessing_Dialog
-import ARCedButonProcessing_Dialog
-import ARCedCallCommonEvent_Dialog
-import ARCedChangeAccess_Dialog
-import ARCedChangeActorClass_Dialog
-import ARCedChangeActorGraphic_Dialog
-import ARCedChangeActorName_Dialog
-import ARCedChangeBlending_Dialog
-import ARCedChangeEquipment_Dialog
-import ARCedChangeFogOpacity_Dialog
-import ARCedChangeFrequency_Dialog
-import ARCedChangeHP_Dialog
-import ARCedChangelGold_Dialog
-import ARCedChangelPartyEquipment_Dialog
-import ARCedChangeMapSettings_Dialog
-import ARCedChangeMaximum_Dialog
-import ARCedChangeParameters_Dialog
-import ARCedChangePartyMember_Dialog
-import ARCedChangePictureColorTone_Dialog
-import ARCedChangeSkills_Dialog
-import ARCedChangeSpeed_Dialog
-import ARCedChangeState_Dialog
-import ARCedChangeStat_Dialog
-import ARCedChangeTextOptions_Dialog
-import ARCedChangeTone_Dialog
-import ARCedChooseActor_Dialog
-import ARCedChooseAudio_Dialog
-import ARCedChooseFogGraphic_Dialog
-import ARCedChooseGraphic_Dialog
-import ARCedChooseSwitchVariable_Dialog
-import ARCedChooseTreasure_Dialog
+import ARCedAnimations_Panel
 import ARCedClasses_Panel
-import ARCedComment_Dialog
 import ARCedCommonEvents_Panel
-import ARCedConditionalBranch_Dialog
-import ARCedControlSelfSwitches_Dialog
-import ARCedControlSwitches_Dialog
-import ARCedControlTimer_Dialog
-import ARCedControlVariables_Dialog
-import ARCedDatabaseMain_Panel
-import ARCedDealDamage_Dialog
 import ARCedEnemies_Panel
-import ARCedEnemyAction_Dialog
-import ARCedEnemyAppearance_Dialog
-import ARCedEnemyTransform_Dialog
-import ARCedErasePicture_Dialog
-import ARCedEventCommands1_Panel
-import ARCedEventCommands2_Panel
-import ARCedEventCommands3_Panel
-import ARCedEventCondition_Dialog
-import ARCedEventEditor_Panel
-import ARCedEventPage_Panel
-import ARCedExpCurve_Dialog
-import ARCedFadeOutAudio_Dialog
-import ARCedForceAction_Dialog
-import ARCedGenerateCurve_Dialog
-import ARCedInputNumber_Dialog
 import ARCedItems_Panel
-import ARCedJump_Dialog
-import ARCedLabel_Dialog
-import ARCedMovePicture_Dialog
-import ARCedMoveRoute_Dialog
-import ARCedNameProcessing_Dialog
-import ARCedRecoverAll_Dialog
-import ARCedRotatePicture_Dialog
-import ARCedScreenShake_Dialog
-import ARCedScriptCall_Dialog
-import ARCedScrollMap_Dialog
-import ARCedSetEventLocation_Dialog
-import ARCedShopGoods_Dialog
-import ARCedShopProcessing_Dialog
-import ARCedShowAnimation_Dialog
-import ARCedShowChoices_Dialog
-import ARCedShowPicture_Dialog
-import ARCedShowText_Dialog
 import ARCedSkills_Panel
-import ARCedSkill_Dialog
 import ARCedStates_Panel
 import ARCedSystem_Panel
 import ARCedTilesets_Panel
-import ARCedTransferEventTilemap_Dialog
-import ARCedTransferPlayerTilemap_Dialog
-import ARCedTransferPlayer_Dialog
-import ARCedTransparentFlag_Dialog
 import ARCedTroops_Panel
-import ARCedWait_Dialog
 import ARCedWeapons_Panel
-import ARCedWeatherEffects_Dialog
+
+class DatabasePage():
+
+	def __init__(self, title, page, index):
+		self.Title = title
+		self.Page = page
+		self.DisplayOrder = index
+
 
 class ARCedTest(wx.App):
 
 	def __init__(self, redirect=False, filename=None):
+		# Create the application and main frame
 		wx.App.__init__(self, redirect, filename)
 		self.frame = wx.Frame(None, wx.ID_ANY, title='ARCed Panel Test', size=(800, 600))
+		self.frame.CenterOnScreen()
+		# Read and parse the .ini file to determine what tabs will be available
+		Config = ConfigParser.SafeConfigParser()
+		Config.read('DatabaseTabs.ini')
+		# Create Notebook control, and dynamically add the defined controls to it
 		nb = wx.Notebook(self.frame, wx.ID_ANY)
-		tabs = ('Actors', 'Classes', 'Skills', 'Items', 'Weapons', 'Armors',
-				'Enemies', 'Troops', 'States', 'Animations', 'Tilesets',
-				'Common Events', 'System')
-		pages = (ARCedActors_Panel.ARCedActors_Panel, ARCedClasses_Panel.ARCedClasses_Panel,
-				 ARCedSkills_Panel.ARCedSkills_Panel, ARCedItems_Panel.ARCedItems_Panel,
-				 ARCedWeapons_Panel.ARCedWeapons_Panel, ARCedArmors_Panel.ARCedArmors_Panel,
-				 ARCedEnemies_Panel.ARCedEnemies_Panel, ARCedTroops_Panel.ARCedTroops_Panel,
-				 ARCedStates_Panel.ARCedStates_Panel, ARCedAnimations_Panel.ARCedAnimations_Panel,
-				 ARCedTilesets_Panel.ARCedTilesets_Panel, ARCedCommonEvents_Panel.ARCedCommonEvents_Panel,
-				 ARCedSystem_Panel.ARCedSystem_Panel)
-		for i in range(len(pages)):
-			nb.AddPage(pages[i](nb), tabs[i], False, i)
+		global DatabasePages
+		DatabasePages = {}
+		for tabName in Config.sections():
+			file = Config.get(tabName, 'file')
+			klass = Config.get(tabName, 'class')
+			index = Config.getint(tabName, 'index')
+			exec('page = ' + file + '.' + klass + '(nb)')
+			DatabasePages[index] = DatabasePage(tabName, page, index)
+		# Sort the list and add each page to the control
+		for i in sorted(DatabasePages.keys()):
+			nb.AddPage(DatabasePages[i].Page, DatabasePages[i].Title)
 		self.frame.Show()
 
+# Create window and execute the main loop
 if __name__ == '__main__':
 	app = ARCedTest()
 	app.MainLoop()
