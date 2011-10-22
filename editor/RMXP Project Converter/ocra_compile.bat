936b26e273f1cfb9864ba8a81d777723917867c6
Ryex <ryexander@gmail.com>
2011-10-22 22:44:50 +0000

    - Finished the project converter, .rxproj can be dragged onto the exe in the file system or into the running program to select the path. also I managed to get the controls to look native instead of those flat plain buttons from windows 98. I had to patch a manifest into my ruby and rubw exes (don't worry I made a back up of them) so it will only work if I compile it with orca on my computer
    - Update to the inno set up file, I put in the lines necessary to register the .arcproj file extension to open with ARCed. side effect of using pyinstaller is that the controls don't look right unless the exe is told to run in compatibility mode so I put a line in the setup file that will had a registry value that will tell the exe to run in XPSP@ compatibility mod if the windows version is vista or above.

---
+++ editor/RMXP Project Converter/ocra_compile.bat
@@ -0,0 +1,37 @@
+@ECHO off
+cls
+
+::setup envierment
+set working=%~dp0
+echo working in %working%
+set "ocra_file=%working%ocrasa.rb"
+set "out_path=%working%bin\"
+set "script=%working%src\program.rbw"
+::set "temp_exe=%working%program.exe"
+set "name=RMXP2ARC.exe"
+set "icon=%working%icon.ico"
+
+echo ====================================================
+echo ** RMXP to ARC Project Converter Compile via Orca **
+echo ====================================================
+echo.
+
+echo.
+echo Deleating old build...
+del /s /q "%out_path%%name%"
+
+:: --console --windows --chdir-first 
+:: run ocra
+echo.
+ruby "%ocra_file%" "%script%" --icon "%icon%" --windows --output "%out_path%%name%" --chdir-first
+
+::echo.
+::echo Moving resultent exe to bin\ dir
+
+::mkdir "%out_path%"
+::ren "%temp_exe%" "%name%"
+::move "%working%%name%" "%out_path%"
+
+echo =========== Finished Build ===========
+
+PAUSE
