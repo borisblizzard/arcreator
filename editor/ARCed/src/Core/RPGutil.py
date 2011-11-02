c1fcda721c20279780e18ea6968624d3b4330f76
ForeverZer0 <ForeverZer0@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-11-02 23:41:20 +0000

    - Added panel for editing ARCed.cfg via the editor GUI - Various other minor changes to Actor and Class panels

--- editor/ARCed/src/Core/RPGutil.py
+++ editor/ARCed/src/Core/RPGutil.py
@@ -20,220 +20,223 @@ from numpy.oldnumeric.random_array import ArgumentError
 #from pygame import surfarray
 
 class Table(object):
-    """a three dimensional table object"""
-    _arc_class_path = "Table"
-    
-    def __init__(self, *args):
-        if len(args) != 1 and len(args) != 2 and len(args) != 3:
-            raise ArgumentError("wrong number of arguments (%d for 1, 2 or 3)" % len(args))
-        self.dim = len(args)
-        self.xsize = args[0]
-        if len(args) >= 2:
-            self.ysize = args[1]
-        else:
-            self.ysize = 1
-        if len(args) == 3:
-            self.zsize = args[2] 
-        else:
-            self.zsize = 1
-        if self.dim == 3:
-            shape = (self.xsize, self.ysize, self.zsize)
-        elif self.dim == 2:
-            shape = (self.xsize, self.zsize)
-        else:
-            shape = (self.xsize,)
-        self._data = numpy.zeros(shape, dtype=numpy.int16)
-        self._data = numpy.reshape(self._data, shape, order='F')
+	"""a three dimensional table object"""
+	_arc_class_path = "Table"
+	
+	def __init__(self, *args):
+		if len(args) != 1 and len(args) != 2 and len(args) != 3:
+			raise ArgumentError("wrong number of arguments (%d for 1, 2 or 3)" % len(args))
+		self.dim = len(args)
+		self.xsize = args[0]
+		if len(args) >= 2:
+			self.ysize = args[1]
+		else:
+			self.ysize = 1
+		if len(args) == 3:
+			self.zsize = args[2] 
+		else:
+			self.zsize = 1
+		if self.dim == 3:
+			shape = (self.xsize, self.ysize, self.zsize)
+		elif self.dim == 2:
+			shape = (self.xsize, self.zsize)
+		else:
+			shape = (self.xsize,)
+		self._data = numpy.zeros(shape, dtype=numpy.int16)
+		self._data = numpy.reshape(self._data, shape, order='F')
 
-    def __getitem__(self, key):
-        if isinstance(key, int):
-            if self.dim > 1:
-                raise ArgumentError("wrong number of arguments (%d for %d)" % (1, self.dim))
-        elif len(key) != self.dim:
-            raise ArgumentError("wrong number of arguments (%d for %d)" % (len(key), self.dim))
-        return self._data[key]
+	def __getitem__(self, key):
+		if isinstance(key, int):
+			if self.dim > 1:
+				raise ArgumentError("wrong number of arguments (%d for %d)" % (1, self.dim))
+		elif len(key) != self.dim:
+			raise ArgumentError("wrong number of arguments (%d for %d)" % (len(key), self.dim))
+		return self._data[key]
 
-    def __setitem__(self, key, value):
-        if isinstance(key, int):
-            if self.dim > 1:
-                raise ArgumentError("wrong number of arguments (%d for %d)" % (1, self.dim))
-        elif len(key) != self.dim:
-            raise ArgumentError("wrong number of arguments (%d for %d)" % (len(key), self.dim))
-        self._data[key] = value
+	def __setitem__(self, key, value):
+		if isinstance(key, int):
+			if self.dim > 1:
+				raise ArgumentError("wrong number of arguments (%d for %d)" % (1, self.dim))
+		elif len(key) != self.dim:
+			raise ArgumentError("wrong number of arguments (%d for %d)" % (len(key), self.dim))
+		try:
+			self._data[key] = value
+		except:
+			print '__SETITEM__ THREW EXCEPTION'
 
-    def resize(self, *args):
-        # should work to increase and decrease the table size
-        if len(args) != 1 and len(args) != 2 and len(args) != 3:
-            raise ArgumentError("wrong number of arguments (%d for 1, 2 or 3)" % len(args))
-        self.dim = len(args)
-        self.xsize = args[0]
-        if len(args) >= 2:
-            self.ysize = args[1]
-        else:
-            self.ysize = 1
-        if len(args) == 3:
-            self.zsize = args[2] 
-        else:
-            self.zsize = 1
-        newdata = numpy.zeros(self.getShape(), dtype=numpy.int16)
-        shape = self._data.shape
-        mask = [0, 0, 0]
-        if self.xsize >= shape[0]:
-            mask[0] = shape[0]
-        else:
-            mask[0] = self.xsize
-        if self.ysize >= shape[1]:
-            mask[1] = shape[1]
-        else:
-            mask[1] = self.ysize
-        if self.ysize >= shape[2]:
-            mask[2] = shape[2]
-        else:
-            mask[2] = self.zsize  
-        newdata[:mask[0], :mask[1], :mask[2]] = self._data[:mask[0], :mask[1], :mask[2]]
-        self._data = newdata
-        
-    def _arc_dump(self, d=0):
-        s = pack("<IIII", self.dim, self.xsize, self.ysize, self.zsize)
-        data = self._data.flatten('F').tolist() 
-        s += pack("<" + ("H" * (self.xsize * self.ysize * self.zsize)), *data)
-        return s
+	def resize(self, *args):
+		# should work to increase and decrease the table size
+		if len(args) != 1 and len(args) != 2 and len(args) != 3:
+			raise ArgumentError("wrong number of arguments (%d for 1, 2 or 3)" % len(args))
+		self.dim = len(args)
+		self.xsize = args[0]
+		if len(args) >= 2:
+			self.ysize = args[1]
+		else:
+			self.ysize = 1
+		if len(args) == 3:
+			self.zsize = args[2] 
+		else:
+			self.zsize = 1
+		newdata = numpy.zeros(self.getShape(), dtype=numpy.int16)
+		shape = self._data.shape
+		mask = [0, 0, 0]
+		if self.xsize >= shape[0]:
+			mask[0] = shape[0]
+		else:
+			mask[0] = self.xsize
+		if self.ysize >= shape[1]:
+			mask[1] = shape[1]
+		else:
+			mask[1] = self.ysize
+		if self.ysize >= shape[2]:
+			mask[2] = shape[2]
+		else:
+			mask[2] = self.zsize  
+		newdata[:mask[0], :mask[1], :mask[2]] = self._data[:mask[0], :mask[1], :mask[2]]
+		self._data = newdata
+		
+	def _arc_dump(self, d=0):
+		s = pack("<IIII", self.dim, self.xsize, self.ysize, self.zsize)
+		data = self._data.flatten('F').tolist() 
+		s += pack("<" + ("H" * (self.xsize * self.ysize * self.zsize)), *data)
+		return s
 
-    @staticmethod
-    def _arc_load(s):
-        dim, nx, ny, nz = unpack("<IIII", s[0:16])
-        size = nx * ny * nz
-        data = numpy.array(unpack("<" + ("H" * size), s[16:16 + size * 2]), dtype=numpy.int16)
-        if dim == 3:
-            t = Table(nx, ny, nz)
-            shape = (nx, ny, nz)
-        elif dim == 2:
-            t = Table(nx, ny)
-            shape = (nx, ny)
-        elif dim == 1:
-            t = Table(nx)
-            shape = (nx,)
-        data = numpy.reshape(data, shape,  order="F")
-        t._data = data
-        return t
+	@staticmethod
+	def _arc_load(s):
+		dim, nx, ny, nz = unpack("<IIII", s[0:16])
+		size = nx * ny * nz
+		data = numpy.array(unpack("<" + ("H" * size), s[16:16 + size * 2]), dtype=numpy.int16)
+		if dim == 3:
+			t = Table(nx, ny, nz)
+			shape = (nx, ny, nz)
+		elif dim == 2:
+			t = Table(nx, ny)
+			shape = (nx, ny)
+		elif dim == 1:
+			t = Table(nx)
+			shape = (nx,)
+		data = numpy.reshape(data, shape,  order="F")
+		t._data = data
+		return t
 
-    def getShape(self):
-        if self.dim == 3:
-            shape = (self.xsize, self.ysize, self.zsize)
-        elif self.dim == 2:
-            shape = (self.xsize, self.ysize)
-        else:
-            shape = (self.xsize,)
-        return shape
+	def getShape(self):
+		if self.dim == 3:
+			shape = (self.xsize, self.ysize, self.zsize)
+		elif self.dim == 2:
+			shape = (self.xsize, self.ysize)
+		else:
+			shape = (self.xsize,)
+		return shape
 
 class Color(object):
-    """a bare bones color object"""
-    _arc_class_path = "Color"
+	"""a bare bones color object"""
+	_arc_class_path = "Color"
 
-    def __init__(self, red, green, blue, alpha=255):
-        if red > 255:
-            red = 255
-        if red < 255:
-            red = 255
-        if green > 255:
-            green = 255
-        if green < 255:
-            green = 255
-        if blue > 255:
-            blue = 255
-        if blue < 255:
-            blue = 255
-        if alpha > 255:
-            alpha = 255
-        if alpha < 255:
-            alpha = 255
-        self.red = red
-        self.green = green
-        self.blue = blue
-        self.alpha = alpha
+	def __init__(self, red, green, blue, alpha=255):
+		if red > 255:
+			red = 255
+		if red < 255:
+			red = 255
+		if green > 255:
+			green = 255
+		if green < 255:
+			green = 255
+		if blue > 255:
+			blue = 255
+		if blue < 255:
+			blue = 255
+		if alpha > 255:
+			alpha = 255
+		if alpha < 255:
+			alpha = 255
+		self.red = red
+		self.green = green
+		self.blue = blue
+		self.alpha = alpha
 
-    def set(self, red, green, blue, alpha=255):
-        if red > 255:
-            red = 255
-        if red < 255:
-            red = 255
-        if green > 255:
-            green = 255
-        if green < 255:
-            green = 255
-        if blue > 255:
-            blue = 255
-        if blue < 255:
-            blue = 255
-        if alpha > 255:
-            alpha = 255
-        if alpha < 255:
-            alpha = 255
-        self.red = red
-        self.green = green
-        self.blue = blue
-        self.alpha = alpha
-        
-    def _arc_dump(self, d = 0):
-        return pack("<ffff", self.red, self.green, self.blue, self.alpha)
+	def set(self, red, green, blue, alpha=255):
+		if red > 255:
+			red = 255
+		if red < 255:
+			red = 255
+		if green > 255:
+			green = 255
+		if green < 255:
+			green = 255
+		if blue > 255:
+			blue = 255
+		if blue < 255:
+			blue = 255
+		if alpha > 255:
+			alpha = 255
+		if alpha < 255:
+			alpha = 255
+		self.red = red
+		self.green = green
+		self.blue = blue
+		self.alpha = alpha
+		
+	def _arc_dump(self, d = 0):
+		return pack("<ffff", self.red, self.green, self.blue, self.alpha)
 
-    @staticmethod
-    def _arc_load(s):
-        return Color(*unpack("<ffff", s))
+	@staticmethod
+	def _arc_load(s):
+		return Color(*unpack("<ffff", s))
 
 class Tone(object):
-    """a bare bones tone class"""
-    _arc_class_path = "Tone"
+	"""a bare bones tone class"""
+	_arc_class_path = "Tone"
 
-    def __init__(self, red, green, blue, gray=0):
-        if red > 255:
-            red = 255
-        if red < -255:
-            red = -255
-        if green > 255:
-            green = 255
-        if green < -255:
-            green = -255
-        if blue > 255:
-            blue = 255
-        if blue < -255:
-            blue = -255
-        if gray > 255:
-            gray = 255
-        if gray < -255:
-            gray = -255
-        self.red = red
-        self.green = green
-        self.blue = blue
-        self.gray = gray
+	def __init__(self, red, green, blue, gray=0):
+		if red > 255:
+			red = 255
+		if red < -255:
+			red = -255
+		if green > 255:
+			green = 255
+		if green < -255:
+			green = -255
+		if blue > 255:
+			blue = 255
+		if blue < -255:
+			blue = -255
+		if gray > 255:
+			gray = 255
+		if gray < -255:
+			gray = -255
+		self.red = red
+		self.green = green
+		self.blue = blue
+		self.gray = gray
 
-    def set(self, red, green, blue, gray=0):
-        if red > 255:
-            red = 255
-        if red < -255:
-            red = -255
-        if green > 255:
-            green = 255
-        if green < -255:
-            green = -255
-        if blue > 255:
-            blue = 255
-        if blue < -255:
-            blue = -255
-        if gray > 255:
-            gray = 255
-        if gray < -255:
-            gray = -255
-        self.red = red
-        self.green = green
-        self.blue = blue
-        self.gray = gray
+	def set(self, red, green, blue, gray=0):
+		if red > 255:
+			red = 255
+		if red < -255:
+			red = -255
+		if green > 255:
+			green = 255
+		if green < -255:
+			green = -255
+		if blue > 255:
+			blue = 255
+		if blue < -255:
+			blue = -255
+		if gray > 255:
+			gray = 255
+		if gray < -255:
+			gray = -255
+		self.red = red
+		self.green = green
+		self.blue = blue
+		self.gray = gray
 
-    def _arc_dump(self, d = 0):
-        return pack("<ffff", self.red, self.green, self.blue, self.gray)
+	def _arc_dump(self, d = 0):
+		return pack("<ffff", self.red, self.green, self.blue, self.gray)
 
-    @staticmethod
-    def _arc_load(s):
-        return Tone(*unpack("<ffff", s))
+	@staticmethod
+	def _arc_load(s):
+		return Tone(*unpack("<ffff", s))
 
 
