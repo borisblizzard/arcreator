c39720fe1b2b77ca78a2fe56abfddabd2722e6ec
ForeverZer0 <ForeverZer0@cc99b174-779b-d867-d7b6-26dcebe170e7>
2011-12-07 05:18:44 +0000

    - Fixed some encoding bugs where I forgot to use "GetTextUTF8" - Fixed some warnings that occurred while comparing ascii strings to utf8 ones - Implemented saving of individual scripts via an added save button to the script toolbar - Fixed and optimized the "IsModified" functions so they were actually telling the truth. It will also revert back to unmodified if the script is changed back again after changes being made - Fixed a possible bug with renaming scripts and the wrong name being returned with GetName() of the script object

--- editor/ARCed Database/Database/ScriptEditor/Script.py
+++ editor/ARCed Database/Database/ScriptEditor/Script.py
@@ -13,30 +13,36 @@ class Script(object):
 		self.LoadScript(path)
 		self.CursorPosition = 0
 		self._modified_text = None
+		self._modified_name = None
 
 	def IsModified( self ):
 		"""Returns True/False if text has been modified from original"""
-		return self._modified_text != None
+		if self._modified_text is None:
+			return False
+		return self._text != self._modified_text
 
-	def ApplyChanges( self, save=False ):
+	def ApplyChanges( self ):
 		"""Sets the original text to that of the modified text"""
 		if self._modified_text is not None:
 			self._text = self._modified_text
-			self._modified = None
-			if save:
-				self.SaveScript()
+			self._modified_text = None
+		if self._modified_name is not None:
+			self._name = self._modified_name 
+			self._modified_name = None
 
 	def GetName( self ):
 		"""Returns the name of the script"""
-		return self._name[5:]
-
-	def ChangeName(self, name ):
-		"""Changes the name of the script"""
-		self._name = name
-
-	def GetRealName( self ):
-		"""Returns the name of the script including the leading indexing numbers"""
+		if self._modified_name is not None:
+			return self._modified_name
 		return self._name
+
+	def GetFileName( self ):
+		"""Returns the name of the script including the leading indexing numbers"""
+		return os.path.basename(self.GetPath())
+
+	def ChangeName( self, name ):
+		if name != self._name:
+			self._modified_name = name
 
 	def GetText( self ):
 		"""Returns the text of the script as a string"""
@@ -48,7 +54,8 @@ class Script(object):
 		"""Sets the new string value of the text, but does not apply it permanently"""
 		if self._readonly:
 			return
-		self._modified_text = text
+		if text != self.GetText():
+			self._modified_text = text
 
 	def GetLines( self ):
 		"""Returns a string list of the scripts text broken into lines"""
@@ -81,7 +88,7 @@ class Script(object):
 		"""Loads the script from path and returns True if successful"""
 		try:
 			self._path = path
-			self._name = os.path.splitext(os.path.basename(path))[0]
+			self._name = os.path.splitext(os.path.basename(path))[0][5:]
 			file = open(path, 'rb')
 			self._text = file.read()
 			file.close()
@@ -90,3 +97,15 @@ class Script(object):
 			Kernel.Log(str.format('Failed to load script at {}', path),
 			  '[ScriptEditor]', True, True)
 		return False
+
+	def SaveScript( self, id ):
+		import codecs
+		filename = ''.join([str(id).zfill(4), '-', self.GetName(), '.rb'])
+		if filename != self.GetFileName():
+			os.remove(self.GetPath())
+			self.SetPath(os.path.join(self.GetDirectory(), filename))
+		self.ApplyChanges()
+		file = open(self.GetPath(), 'wb')
+		file.write(codecs.BOM_UTF8)
+		file.write(self.GetText().encode('utf-8'))
+		file.close()
