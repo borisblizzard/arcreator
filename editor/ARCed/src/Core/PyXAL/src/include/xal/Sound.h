fd68cc164fbb7d61de8e5e7dd669b3004278af09
Boris Mikic <boris.blizzard@gmail.com>
2012-05-22 15:41:18 +0000

    - updated PyXAL bin/lib/include (more important fixes)

--- editor/ARCed/src/Core/PyXAL/src/include/xal/Sound.h
+++ editor/ARCed/src/Core/PyXAL/src/include/xal/Sound.h
@@ -1,6 +1,6 @@
 /// @file
 /// @author  Boris Mikic
-/// @version 2.0
+/// @version 2.7
 /// 
 /// @section LICENSE
 /// 
@@ -44,7 +44,7 @@ namespace xal
 		Format getFormat();
 		bool isStreamed();
 
-		int readRawData(unsigned char** output);
+		int readPcmData(unsigned char** output);
 
 	protected:
 		hstr name;
