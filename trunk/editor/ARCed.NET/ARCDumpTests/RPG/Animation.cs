61ef6d29322dc1b52d0b298c81c657705a6ee1ce
ForeverZer0 <ForeverZer0@cc99b174-779b-d867-d7b6-26dcebe170e7>
2012-07-03 21:24:59 +0000

    - Added important files that should have already been committed, removed some worthless files that should never have been, added some folders/files to ignore list - Added Documentation project - Added ARCDumpTests project, though it will likely be removed soon.

---
+++ trunk/editor/ARCed.NET/ARCDumpTests/RPG/Animation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPG
+{
+	public class Animation
+	{
+
+		public int id { get; set; }
+		public string name { get; set; }
+		public string animation_name { get; set; }
+		public int animation_hue { get; set; }
+		public int position { get; set; }
+		public int frame_max { get; set; }
+		public List<dynamic> frames { get; set; }
+		public List<dynamic> timings { get; set; }
+
+		public Animation()
+		{
+			id = 0;
+			name = "";
+			animation_name = "";
+			animation_hue = 0;
+			position = 1;
+			frame_max = 1;
+			frames = new List<dynamic>() {  };
+			timings = new List<dynamic>() {  };
+		}
+
+		public class Frame
+		{
+			public int cell_max { get; set; }
+			public Table cell_data { get; set; }
+
+			public Frame()
+			{
+				cell_max = 0;
+				cell_data = new Table(0, 0);
+			}
+		}
+
+		public class Timing
+		{
+			public int frame { get; set; }
+			public AudioFile se { get; set; }
+			public int flash_scope { get; set; }
+			public Color flash_color { get; set; }
+			public int flash_duration { get; set; }
+			public int condition { get; set; }
+
+			public Timing()
+			{
+				frame = 0;
+				se = new AudioFile("", 80);
+				flash_scope = 0;
+				flash_color = new Color();
+				flash_duration = 5;
+				condition = 0;
+			}
+		}
+	}
+}
---
+++ trunk/editor/ARCed.NET/ARCDumpTests/RPG/State.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace RPG
+{
+	public class State
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public int animation_id { get; set; }
+		public int restriction { get; set; }
+		public bool nonresistance { get; set; }
+		public bool zero_hp { get; set; }
+		public bool cant_get_exp { get; set; }
+		public bool cant_evade { get; set; }
+		public bool slip_damage { get; set; }
+		public int rating { get; set; }
+		public int hit_rate { get; set; }
+		public int maxhp_rate { get; set; }
+		public int maxsp_rate { get; set; }
+		public int str_rate { get; set; }
+		public int dex_rate { get; set; }
+		public int agi_rate { get; set; }
+		public int int_rate { get; set; }
+		public int atk_rate { get; set; }
+		public int pdef_rate { get; set; }
+		public int mdef_rate { get; set; }
+		public int eva { get; set; }
+		public bool battle_only { get; set; }
+		public int hold_turn { get; set; }
+		public int auto_release_prob { get; set; }
+		public int shock_release_prob { get; set; }
+		public List<dynamic> guard_element_set { get; set; }
+		public List<dynamic> plus_state_set { get; set; }
+		public List<dynamic> minus_state_set { get; set; }
+
+		public State()
+		{
+			id = 0;
+			name = "";
+			animation_id = 0;
+			restriction = 0;
+			nonresistance = false;
+			zero_hp = false;
+			cant_get_exp = false;
+			cant_evade = false;
+			slip_damage = false;
+			rating = 5;
+			hit_rate = 100;
+			maxhp_rate = 100;
+			maxsp_rate = 100;
+			str_rate = 100;
+			dex_rate = 100;
+			agi_rate = 100;
+			int_rate = 100;
+			atk_rate = 100;
+			pdef_rate = 100;
+			mdef_rate = 100;
+			eva = 0;
+			battle_only = true;
+			hold_turn = 0;
+			auto_release_prob = 0;
+			shock_release_prob = 0;
+			guard_element_set = new List<dynamic>();
+			plus_state_set = new List<dynamic>();
+			minus_state_set = new List<dynamic>();
+		}
+	}
+}
