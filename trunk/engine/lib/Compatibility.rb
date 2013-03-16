33cee77b9225d585983a5b5dbe1b5e062ea4d938
boris.blizzard <boris.blizzard@cc99b174-779b-d867-d7b6-26dcebe170e7>
2013-03-16 16:55:14 +0000

    - lib/dll update

--- trunk/engine/lib/Compatibility.rb
+++ trunk/engine/lib/Compatibility.rb
@@ -8,17 +8,17 @@ class Win32API
   def initialize(dllname, func, import, export = "0", calltype = :stdcall)
     import = import.join('') if import.is_a?(Array)
     export = export.join('') if export.is_a?(Array)
-    @export = export.clone
     import.upcase!
     export.upcase!
+    @export = export.clone
     @api = Win32::API.new(func, import, export, dllname)
   end
 
   def call(*args)
     result = @api.call(*args)
-    result -= LIMIT_L if @export == 'l' && result >= LIMIT_L / 2
-    result -= LIMIT_S if @export == 's' && result >= LIMIT_S / 2
-    result -= LIMIT_C if @export == 'c' && result >= LIMIT_C / 2
+    result -= LIMIT_L if @export == 'L' && result >= LIMIT_L / 2
+    result -= LIMIT_S if @export == 'S' && result >= LIMIT_S / 2
+    result -= LIMIT_C if @export == 'C' && result >= LIMIT_C / 2
     return result
   end
 
